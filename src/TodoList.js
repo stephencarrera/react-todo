526869f82fb3e716c040220f44c748b6e6ba83fd
Stephen Carrera <stephencarrera@users.noreply.github.com>
2018-01-03 23:35:52 -0800

    styled list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Todo from "./Todo";
+import styled from "styled-components";
+
+const ListStyle = styled.div`
+  border: 1px solid purple;
+  height: 100%;
+  width: 33%;
+  color: white;
+  background-color: orange;
+  font-size: 24px;
+`;
 
 class TodoList extends Component {
   render() {
     return (
       <div>
         <p>hello from the list</p>
-        <Todo />
+        <ListStyle>
+          <Todo />
+        </ListStyle>
       </div>
     );
   }
