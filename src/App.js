c12b7f2487a171d6925ddfa49bad13d9768286bf
Stephen Carrera <stephencarrera@users.noreply.github.com>
2018-01-07 11:18:07 -0800

    restart Todo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from "react";
-import TodoList from "./TodoList";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const LinkStyle = styled.div`
-  color: white;
-  font-size: 20px;
-  padding: 10px 20px 10px 20px;
-  text-decoration: none;
-  width: 200px;
-`;
+import "./App.css";
 
 const App = () => (
   <div className="App">
-    <div className="App-header">
-      <h2>Todo App with router and styled-components</h2>
-      <LinkStyle>
-        <Link to="todos/new">Add</Link>
-      </LinkStyle>
-      <LinkStyle>
-        <Link to="/todos">Show</Link>
-      </LinkStyle>
-    </div>
-    <TodoList />
+    <h2>My Todos</h2>
   </div>
 );
 
